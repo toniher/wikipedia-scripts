72084416eabaf2cbdefd71f922785874afeb3e6d
Toni Hermoso Pulido <toniher@cau.cat>
2015-02-15 15:22:12 +0100

    go on

--- category-report-status.pl
+++ category-report-status.pl
@@ -10,33 +10,69 @@ use utf8;
 use 5.010;
 binmode STDOUT, ":utf8";
 
-# GetOpt
+#TODO: Load stuff via JSON
 
 # Category
+my $category = "Category:Bioinformatics";
+my $depth = 1; # Maximum depth of subcategories
+my @exclude = ();
+
 # Language
 
 my $mw = MediaWiki::API->new();
 # URL
-$mw->{config}->{api_url} = 'http://en.wikipedia.org/w/api.php';
+$mw->{config}->{api_url} = 'https://en.wikipedia.org/w/api.php';
 
-# get a list of articles in category
-my $articles = $mw->list ( {
-action => 'query',
-list => 'categorymembers',
-cmtitle => 'Category:Perl',
-cmlimit => 'max' } )
-|| die $mw->{error}->{code} . ': ' . $mw->{error}->{details};
+proceed_category( $category, $mw, 0 );
 
-# and print the article titles
-foreach (@{$articles}) {
-	print "$_->{title}\n";
+
+sub proceed_category {
+
+	my $cat = shift;
+	my $mw = shift;
+	my $step = shift;
+
+	print $step, "\n";
+	
+	if ( $step > $depth ) {
+		return;
+	}
+	
+	# get a list of articles in category
+	my $articles = $mw->list ( {
+		action => 'query',
+		list => 'categorymembers',
+		cmtitle => $cat,
+		cmlimit => 'max' } )
+	|| die $mw->{error}->{code} . ': ' . $mw->{error}->{details};
+
+	my $categories = $mw->list ( {
+		action => 'query',
+		list => 'categorymembers',
+		cmtitle => $cat,
+		cmtype => 'subcat',
+		cmlimit => 'max' } )
+	|| die $mw->{error}->{code} . ': ' . $mw->{error}->{details};
+
+	# and print the article titles
+	foreach (@{$articles}) {
+		print "$_->{title}\n";
+		#get_length( $_->{title}, $mw );
+		print "\n";
+		# Detect Category, proceed.
+	}
+	
+	foreach (@{$categories}) {
+		print "CAT: ".$_->{title}."\n";
+		proceed_category( $_->{title}, $mw, $step + 1 );
+		sleep(1);
+	}
 }
-
-
 
 # Length of page
 sub get_length {
  
+ 	# TODO: Change to MediaWiki::API
 	my $site = shift;
 	my $entry = shift;
 	
