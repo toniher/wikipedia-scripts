6f11c45a5b1f557decebf4aedde0c70e6c20466b
Toni Hermoso Pulido <toniher@cau.cat>
2016-03-16 09:44:11 +0100

    start JSON processing script

---
+++ wikidata-dumpjson-checkNonEqualLang.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/env perl
+
+use JSON;
+use Parallel::ForkManager;
+ 
+# Directory of dumps
+my $dir = shift;
+my $procs = shift // 4;
+
+# Directory with Wikidata pieces
+if ( ! defined( $dir ) ) {
+	exit;
+}
+
+opendir( DIR, $dir ) or die $!;
+
+my $fork= new Parallel::ForkManager( $procs );
+
+while ( my $file = readdir(DIR) ) {
+	
+	if ( $file=~/^\./ ) { next; }
+	
+	my $pid= $fork->start and next;
+
+	processJSONfile( $dir."/".$file );
+	
+	$fork->finish;
+}
+
+$fork->wait_all_children;
+
+
+sub processJSONfile {
+	
+	my $file = shift;
+	
+	# Process JSON file
+	# Line by line is a JSON piece
+}
+
