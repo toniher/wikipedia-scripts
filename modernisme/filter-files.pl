dbe2e78e3b5c1817562d899cd4dcbe7f7eae2965
Toni Hermoso Pulido <toniher@cau.cat>
2017-07-26 22:28:56 +0200

    filter files

---
+++ modernisme/filter-files.pl
@@ -0,0 +1,85 @@
+#!usr/bin/env perl
+
+
+my $file = shift;
+my $dirout = shift // "sep" ;
+my $map = shift // 4;
+
+# Directory with Wikidata pieces
+if ( ! defined( $file ) ) {
+	exit;
+}
+
+if ( ! -d $dirout ) {
+	mkdir( $dirout );
+}
+
+my @keys = processFile( $file, $map );
+
+foreach my $key ( @keys ) {
+	
+	my $fileout = $file;
+	$fileout =~ s/\.csv//g;
+	
+	my $finkey = $key;
+	$finkey =~ s/\s/_/g;
+	$finkey =~ s/\'//g;
+	
+	open( FILEOUT, ">".$dirout."/".$fileout.".".$finkey.".csv" );
+	
+	my $iter = 0;
+	
+	open( FILE, $file );
+	
+	while (<FILE>) {
+		
+		if ( $iter == 0 ) {
+			print FILEOUT $_;
+		} else {
+			
+			my @split = split( /\t/, $_ );
+			
+			if ( $split[ $map ] eq $key ) {
+				print FILEOUT $_;
+			}
+		}
+		
+		
+		$iter++;
+	}
+	
+	close( FILE );
+	
+	close( FILEOUT );
+	
+}
+
+sub procesFile  {
+	
+	my $fils = shift;
+	my $map = shift;
+	
+	my @arr;
+	
+	open( FILE, $file );
+	
+	while (<FILE>) {
+		
+		my @split = split( /\t/, $_ );
+		
+		push( @arr, $split[$map] );
+		
+	}
+	
+	close( FILE );	
+	
+	return uniq( @arr );
+	
+}
+
+
+
+sub uniq {
+	my %seen;
+	return grep { !$seen{$_}++ } @_;
+}
